c724900b9cb09637d371535a3cc1bd7c5e730ec2
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-23 15:58:31 +0200

    ✨ Make script actually usable by other people
    Add customizable constants at the top of the file

--- you-will-work-on.py
+++ you-will-work-on.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+
+PROJECTS_DIRECTORY = "/mnt/d/projects"
+ARCHIVED_PROJECTS_DIRECTORY = "/mnt/d/projects/.archived"
+# This will be executed with one position argument, the chosen project's directory.
+CODE_EDITOR_COMMAND = "code"
+
 from typing import *
-from os import listdir, rename
+from os import listdir, rename, path
 from shutil import rmtree
 from os.path import abspath
 import random
 from subprocess import run
 
 i_dont_want_to: List[str] = []
-projects = [p for p in listdir("/mnt/d/projects") if not p.startswith(".")]
+projects = [p for p in listdir(PROJECTS_DIRECTORY) if not p.startswith(".")]
 
 # print("Got projects:\n" + "\n- ".join(projects))
 
@@ -33,7 +39,7 @@ while True:
             print("- Thought so.")
     elif ans.startswith("archive"):
         print("- Okay, moving this project to .archived/")
-        rename(f"/mnt/d/projects/{selected}", f"/mnt/d/projects/.archived/{selected}")
+        rename(path.join(PROJECTS_DIRECTORY, selected), path.join(ARCHIVED_PROJECTS_DIRECTORY, selectd))
     else:
         run(["code", abspath(selected)], shell=True)
         break
